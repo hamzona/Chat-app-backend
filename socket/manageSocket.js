74b30bbee979c898270640d6683cded1d47fa380
hamzona <hamzadjedovic2000@gmail.com>
2023-08-09 23:34:16 +0200

    socket fix

--- socket/manageSocket.js
+++ socket/manageSocket.js
@@ -2,8 +2,10 @@ const manageSocket = async (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
 
-  socket.on("send-message", ({ recipients, message, nickname }) => {
-    socket.broadcast.to(id).emit("recive-message", { message, nickname });
+  socket.on("send-message", ({ recipients, message, nickname, chatId }) => {
+    socket.broadcast
+      .to(id)
+      .emit("recive-message", { message, nickname, chatId });
   });
 };
 
